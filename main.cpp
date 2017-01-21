b4bb37ef814cd261ca4f01a4c9a531ef5db33d45
knight42 <anonymousknight96@gmail.com>
2017-01-22 01:40:56 +0800

    Send current timezone

--- main.cpp
+++ main.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include <sstream>
 #include <string>
+#include <array>
+#include <memory>
 
 #include <ctime>
 #include <cstring>
@@ -12,20 +14,35 @@
 
 using std::string;
 using std::cout;
+using std::cerr;
 using std::endl;
 
 class Downloader {
 public:
     Downloader () {
         curl = curl_easy_init();
+        if (!curl) {
+            cerr << "Failed to get CURL easy handle" << endl;
+            exit(-1);
+        }
+        chunk = NULL;
     }
     ~Downloader () {
         curl_easy_cleanup(curl);
+        curl_slist_free_all(chunk);
     }
 
-    const string& download(const string &url) {
+    const string& download(const string &url, string tz) {
+        size_t slash = tz.find('/');
+        if (slash != string::npos) {
+            tz.replace(slash, 1, "%2F");
+        }
         CURLcode code;
-        code = curl_easy_setopt(curl, CURLOPT_URL, url.c_str());
+        std::ostringstream cookie;
+        cookie << "Cookie: tz=" << tz << ";";
+        chunk = curl_slist_append(chunk, cookie.str().c_str());
+        curl_easy_setopt(curl, CURLOPT_HTTPHEADER, chunk);
+        curl_easy_setopt(curl, CURLOPT_URL, url.c_str());
         curl_easy_setopt(curl, CURLOPT_FOLLOWLOCATION, 1L);
         curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, write_data);
         curl_easy_setopt(curl, CURLOPT_WRITEDATA, &buffer);
@@ -39,6 +56,7 @@ public:
 
 private:
     CURL *curl;
+    struct curl_slist *chunk;
     string buffer;
 
     static size_t write_data(char *ptr, size_t size, size_t nmemb, string *stream) {
@@ -49,6 +67,43 @@ private:
     }
 };
 
+
+typedef std::istringstream sstream;
+
+sstream exec(const char* cmd) {
+    const size_t bufsiz = 256;
+    std::array<char, bufsiz> buffer;
+    std::string result;
+    std::shared_ptr<FILE> pipe(popen(cmd, "r"), pclose);
+    if (!pipe) throw std::runtime_error("popen() failed!");
+    while (!feof(pipe.get())) {
+        if (fgets(buffer.data(), bufsiz, pipe.get()) != NULL)
+            result += buffer.data();
+    }
+    return sstream(result);
+}
+
+string getTZ() {
+    // Sample output:
+    // ...
+    //        Time zone: Asia/Shanghai (CST, +0800)
+    // ...
+    sstream output = exec("timedatectl");
+    string line;
+    string res("");
+    while(std::getline(output, line)) {
+        if (line.find("Time zone") == string::npos) {
+            continue;
+        }
+        size_t colon = line.find(':');
+        size_t bracket = line.find('(');
+        if (colon == string::npos || bracket == string::npos) {
+            break;
+        }
+        res = line.substr(colon + 2, bracket - colon - 3);
+    }
+    return res;
+}
 
 int main(int argc, char *argv[]) {
     char url[100];
@@ -66,12 +121,12 @@ int main(int argc, char *argv[]) {
     strftime(date, 16, "%F", tm);
 
     Downloader d;
-    string data(d.download(url));
+    string data(d.download(url, getTZ()));
     if (data.empty()) {
         return -1;
     }
 
-    std::istringstream iss(data);
+    sstream iss(data);
 
     const char *pat = "data-count=";
     const char *cnt = "0";
